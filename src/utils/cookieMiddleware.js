f3c7bac1d1cc2866d4a56503c6cfed750d35c8c5
Timo Deuschle <github@timodeuschle.de>
2019-12-29 23:21:46 +0100

    set cookie expiry to 1 day

--- src/utils/cookieMiddleware.js
+++ src/utils/cookieMiddleware.js
@@ -1,6 +1,11 @@
 import reduxCookiesMiddleware, {getStateFromCookies} from 'redux-cookies-middleware';
+import Cookies from 'js-cookie'
 
 const paths = {'auth.token': {name: 'access_token'}};
 const emptyState = {auth: {token: undefined}};
-export const initialState = getStateFromCookies(emptyState, paths);
-export const createCookieMiddleware = () => reduxCookiesMiddleware(paths);
+const options = {
+  setCookie: (name, value) => Cookies.set(name, value, { expires: 1, path: '/', secure: false })
+}
+export const initialState = getStateFromCookies(emptyState, paths, name => Cookies.get(name));
+
+export const createCookieMiddleware = () => reduxCookiesMiddleware(paths, options);
